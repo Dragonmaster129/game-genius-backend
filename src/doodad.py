563353480f0b6d57e8b080dd776bbf95931b9990
Dragonmaster129 <dragons4756@gmail.com>
2022-06-08 07:46:39 -0600

    added doodad and described a little bit what it should do.
    Signed-off-by: Dragonmaster129 <dragons4756@gmail.com>

---
+++ src/doodad.py
@@ -0,0 +1,6 @@
+def doodad(data, cash=None, cashflow=None, category=None):
+    # data = self.data
+    # cash = how much you need to pay in now
+    # cashflow = how much you pay in monthly and if there is a cost then you need to add it to card
+    # category = which area does the doodad count in, such as, Mortgage, Car Payment, etc.
+    pass
