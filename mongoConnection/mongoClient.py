61c36c7dd7a0c6231bd5c04fa5e2fe7e74c3bedc
Dragonmaster129 <dragons4756@gmail.com>
2022-06-15 08:45:14 -0600

    moved the mongoClient.py and made it a function
    Signed-off-by: Dragonmaster129 <dragons4756@gmail.com>

---
+++ mongoConnection/mongoClient.py
@@ -0,0 +1,38 @@
+from pymongo import MongoClient
+
+
+def client():
+    mo_c = MongoClient()
+
+    db = mo_c.cashflowDB
+
+    # collections = db.list_collection_names()
+    # print("collections:", collections)
+
+    # for i in collections:
+    #     col = db[i]
+    #     print("Collection:", i)
+    #     for x in col.find({}):
+    #         ID = x.pop("_id")
+    #         try:
+    #             x.pop("pwd")
+    #         except KeyError:
+    #             print(x["description"])
+    #         # print(x)
+
+    col = db["doodad"]
+
+    # How to Insert into the collection in the db
+    # insert = "n"
+    # if insert == "y":
+    #     insertDict = {
+    #         "description": """You Can't Say No
+    # A co-worker asks you to buy cookies
+    # to benefit her child's softball team
+    # Pay: $20""",
+    #         "cash": 20
+    #     }
+    #     print(insertDict)
+    #     x = col.insert_one(insertDict)
+
+    return db
