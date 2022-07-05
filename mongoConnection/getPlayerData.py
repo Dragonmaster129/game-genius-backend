02885006a18a69b5f8feadb6e72f7ceb195a74f4
Dragonmaster129 <dragons4756@gmail.com>
2022-07-05 10:37:55 -0600

    changed where the removal of _id was
    Signed-off-by: Dragonmaster129 <dragons4756@gmail.com>

--- mongoConnection/getPlayerData.py
+++ mongoConnection/getPlayerData.py
@@ -5,10 +5,8 @@ def getPlayerData(email):
     db = mongoClient.client("cashflowDB")
     try:
         playerData = db["player"].find({"email": email})[0]["playerData"]
-        playerData.pop("_id")
         return playerData
     except KeyError:
         resetPlayer.initializePlayerData(email)
         playerData = db["player"].find({"email": email})[0]["playerData"]
-        playerData.pop("_id")
         return playerData
--- sampledata/initData.py
+++ sampledata/initData.py
@@ -4,4 +4,6 @@ from mongoConnection import mongoClient
 def getInitData(profession):
     db = mongoClient.client("cashflowDB")
     initialData = db["initialData"]
-    return initialData.find({"profession": profession})[0]
+    returnData = initialData.find({"profession": profession})[0]
+    returnData.pop("_id")
+    return returnData
